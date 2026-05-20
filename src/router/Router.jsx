e3209ca92edb92249f6e194a2d74c2f184ae9529
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2026-05-20 11:14:27 +0600

    feat: Course, Learn new route

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -6,34 +6,49 @@ import Blogs from "@/pages/Blogs/Blogs";
 import Register from "@/pages/Auth/Register/Register";
 import AuthLayout from "@/layouts/AuthLayout";
 import Login from "@/pages/Auth/Login/Login";
+import Courses from "@/pages/Courses/Courses";
+import CourseDetails from "@/pages/Courses/CourseDetails";
+import Learn from "@/pages/Learn/Learn";
+
 export const Router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     Component: RootLayout,
     children: [
       {
         index: true,
-        Component: Home
+        Component: Home,
       },
       {
-        path: 'blogs',
-        Component: Blogs
+        path: "blogs",
+        Component: Blogs,
       },
-
-    ]
+      {
+        path: "courses",
+        Component: Courses,
+      },
+      {
+        path: "courses/:slug",
+        Component: CourseDetails,
+      },
+      {
+        path: "learn/:slug/:lessonId",
+        Component: Learn,
+      },
+    ],
   },
-  { 
-    path: '/',
+  {
+    path: "/",
     Component: AuthLayout,
     children: [
       {
-        path: 'login',
-        Component: Login
+        path: "login",
+        Component: Login,
       },
       {
-        path: 'register',
-        Component: Register
-      }
-    ]
-  }
+        path: "register",
+        Component: Register,
+      },
+    ],
+  },
 ]);
